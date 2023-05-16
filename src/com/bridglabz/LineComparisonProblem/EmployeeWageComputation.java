73ab42448cb78ebe25034fc15925c15a9600ded5
Kuldeep Kumar <kuldeepkumar17feb@gmail.com>
2023-05-16 18:41:56 +0530

    Added: Welcome to employee computation

---
+++ src/com/bridglabz/LineComparisonProblem/EmployeeWageComputation.java
@@ -0,0 +1,6 @@
+package com.bridglabz.LineComparisonProblem;
+public class EmployeeWageComputation {
+    public static void main(String[] args) {
+        System.out.println("Welcome to employee wage computation Program.");
+    }
+}
