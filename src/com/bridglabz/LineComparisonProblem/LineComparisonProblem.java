351613f40661cd82c1c93b06d5a9aef84cddacf5
Kuldeep Kumar <kuldeepkumar17feb@gmail.com>
2023-05-16 12:17:58 +0530

    Added:Start with displaying program

---
+++ src/com/bridglabz/LineComparisonProblem/LineComparisonProblem.java
@@ -0,0 +1,7 @@
+package com.bridglabz.LineComparisonProblem;
+
+public class LineComparisonProblem {
+    public static void main(String[] args) {
+        System.out.println("Welcome to line computation program");
+    }
+}
